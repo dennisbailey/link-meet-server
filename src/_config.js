3d8f79ed5033075dca8bf06728f2bdeea5b682ee
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-27 14:23:18 -0600

    added an env variable

--- src/_config.js
+++ src/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   test: 'mongodb://localhost/link-meat',
   development: 'mongodb://localhost/link-meat',
-  production: 'mongodb://heroku_31f0dcgs:ca4qba84avk0tcsl24kl986l54@ds021681.mlab.com:21681/heroku_31f0dcgs'
+  production: process.env.MONGODB_URI
 };
 
 config.TOKEN_SECRET = '\x07q\xa1\xb0\xa0\xa7x\xda\xb2\xa9+g|\xd5\x9d\xd9\x9f\x12\xc4-I\x12Q\xfc';
