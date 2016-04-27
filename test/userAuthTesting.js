1af7d09e32bce85616b58a97f9943d4dd5561b9c
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-27 15:00:22 -0600

    added login testing

--- test/userAuthTesting.js
+++ test/userAuthTesting.js
@@ -86,30 +86,30 @@ describe('Auth Routes', function() {
       });
     
     });
-  
+    
+    
+  it('should login a user', function(done) {
+    chai.request(server)
+    .post('/auth/login')
+    .send({
+      email: 'michael@herman.com',
+      password: 'test'
+    })
+    .end(function(err, res) {
+      res.status.should.equal(200);
+      res.type.should.equal('application/json');
+      res.body.should.be.a('object');
+      res.body.should.have.property('status');
+      res.body.should.have.property('data');
+      
+      res.body.status.should.equal('success');
+      
+      res.body.data.should.be.a('object');
+      res.body.data.token.should.be.a('string');
+      res.body.data.user.email.should.equal('michael@herman.com');
+      done();
+    });
   });
-
-//   describe('/POST auth/login', function() {
-//     it('should login a user', function(done) {
-//       chai.request(server)
-//       .post('/auth/login')
-//       .send({
-//         email: 'michael@herman.com',
-//         password: 'test'
-//       })
-//       .end(function(err, res) {
-//         res.status.should.equal(200);
-//         res.type.should.equal('application/json');
-//         res.body.should.be.a('object');
-//         res.body.should.have.property('status');
-//         res.body.should.have.property('data');
-//         res.body.status.should.equal('success');
-//         res.body.data.should.be.a('object');
-//         res.body.data.token.should.be.a('string');
-//         res.body.data.user.should.equal('michael@herman.com');
-//         done();
-//       });
-//     });
-//   });
+});
 
 });
