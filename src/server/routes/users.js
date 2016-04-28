506d54f29ce833b9ba51eda900a4b7c7d67247ed
Garan Guillory <garanguillory@gmail.com>
2016-04-28 14:54:24 -0600

    removed cURL comments

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,19 +2,6 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/users.js');
 
-// curl -H "Content-Type: application/json" -X POST -d '{"name":"Mark Cuban","email":"markcuban@mavs.com", "password":"markcuban"}' http://localhost:3000/auth/register
-
-// curl -H "Content-Type: application/json" -X POST -d '{"name":"Mark Cuban","email":"markcuban@mavs.com", "password":"markcuban"}' http://localhost:3000/login
-
-// curl -H "Content-Type: application/json" http://localhost:3000/user/5722472bc41e9644c7c3cc36
-
-// curl -H '{"Content-Type": "application/json", "authorization":"<insert token here>"}' -X PUT -d '{"name":"<insert group name here>","url":"<insert group url here>","photoUrl":"<insert photoUrl here>"}' http://localhost:3000/user/5722472bc41e9644c7c3cc36/group
-
-// curl -H "Content-Type: application/json" -X PUT -d '{"name":"","notes":""}' http://localhost:3000/user/5722472bc41e9644c7c3cc36/people
-
-// curl -H "Content-Type: application/json" -X DELETE http://localhost:3000/user/5722472bc41e9644c7c3cc36/people
-
-
 
 // Update Current User by adding a new group
   router.put('/:id/group', function(req,res,next) {
